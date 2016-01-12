8cece935c946bd0c7c68f93d3b10858630d27c95
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-12 08:25:32 -0500

    committing everything

--- lisp/setup.el
+++ lisp/setup.el
@@ -1,3 +1,4 @@
+(require 'vc-git)
 (setq ta-rubrics '(("homework" . (("\"technical\"" . 0.7)
 				  ("\"presentation\"" . 0.3)))
 		   ("exam" . (("\"technical\"" . 0.7)
@@ -6,6 +7,6 @@
 		   ("participation" . (("\"participation\"" . 1.0)))))
 
 ;; start dft-mode
-(load-file "../dft-book/dft.el")
+(load-file (expand-file-name  "dft-book/dft.el" (vc-git-root ".")))
 (dft-book-mode)
 (message "course setup.el loaded")
