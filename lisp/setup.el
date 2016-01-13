e0478641f5f8bae316a857b55607df3e9c0823d3
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-12 19:10:30 -0500

    update submodules

--- lisp/setup.el
+++ lisp/setup.el
@@ -6,6 +6,15 @@
 		   ("multiple-choice" . (("\"participation\"" . 1.0)))
 		   ("participation" . (("\"participation\"" . 1.0)))))
 
+;; update submodules
+(with-current-directory (cond
+			 ((boundp 'tq-course-directory)
+			  tq-course-directory)
+			 ((boundp 'ta-course-dir)
+			  ta-course-dir))
+			(mygit "git submodule init")
+			(mygit "git submodule update"))
+
 ;; start dft-mode
 (load-file (expand-file-name  "dft-book/dft.el"
 			      (cond
