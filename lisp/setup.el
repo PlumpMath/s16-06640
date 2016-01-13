82c1758dc345e0f881f9c516a80429286765e445
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-12 19:07:42 -0500

    change to user course directory or admin directory

--- lisp/setup.el
+++ lisp/setup.el
@@ -7,6 +7,11 @@
 		   ("participation" . (("\"participation\"" . 1.0)))))
 
 ;; start dft-mode
-(load-file (expand-file-name  "dft-book/dft.el" ta-course-dir))
+(load-file (expand-file-name  "dft-book/dft.el"
+			      (cond
+			       ((boundp tq-course-directory)
+				tq-course-directory)
+			       ((boundp ta-course-dir)
+				ta-course-dir))))
 (dft-book-mode)
 (message "course setup.el loaded")
