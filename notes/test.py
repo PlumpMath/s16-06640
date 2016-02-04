160f77e916cfd24c1fbe1586da131695b28b9dcc
John Kitchin <jkitchin@cmu.edu>
2016-02-04 06:52:30 -0500

    my changes

---
+++ notes/test.py
@@ -0,0 +1,13 @@
+import os
+
+print os.path.exists('DFT+U-lecture.pdf')
+
+# Run C-c C-c
+
+for i in range(6):
+    print i
+    for j in range(4):
+        print i*j
+    print i + j
+
+print 'done'
