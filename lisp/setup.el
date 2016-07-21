edeb62fab789d3a0282689dc4a02cfc1393d1e19
John Kitchin <jkitchin@andrew.cmu.edu>
2016-07-20 20:24:29 -0400

    committing everything

--- lisp/setup.el
+++ lisp/setup.el
@@ -1,39 +1,39 @@
-(require 'vc-git)
-(setq ta-rubrics '(("homework" . (("\"technical\"" . 0.7)
-				  ("\"presentation\"" . 0.3)))
-		   ("exam" . (("\"technical\"" . 0.7)
-			      ("\"presentation\"" . 0.3)))
-		   ("multiple-choice" . (("\"participation\"" . 1.0)))
-		   ("participation" . (("\"participation\"" . 1.0)))))
+;; (require 'vc-git)
+;; (setq ta-rubrics '(("homework" . (("\"technical\"" . 0.7)
+;; 				  ("\"presentation\"" . 0.3)))
+;; 		   ("exam" . (("\"technical\"" . 0.7)
+;; 			      ("\"presentation\"" . 0.3)))
+;; 		   ("multiple-choice" . (("\"participation\"" . 1.0)))
+;; 		   ("participation" . (("\"participation\"" . 1.0)))))
 
 ;; update submodules
-(with-current-directory (cond
-			 ((and (boundp 'tq-course-directory)
-			       tq-course-directory)
-			  tq-course-directory)
-			 ((and (boundp 'ta-course-dir)
-			       ta-course-dir)
-			  ta-course-dir))
-			(mygit "git submodule init")
-			(mygit "git submodule update"))
+;; (with-current-directory (cond
+;; 			 ((and (boundp 'tq-course-directory)
+;; 			       tq-course-directory)
+;; 			  tq-course-directory)
+;; 			 ((and (boundp 'ta-course-dir)
+;; 			       ta-course-dir)
+;; 			  ta-course-dir))
+;; 			(mygit "git submodule init")
+;; 			(mygit "git submodule update"))
 
 ;; start dft-mode
-(load-file (expand-file-name  "dft-book/dft.el"
-			      (cond
-			       ((and (boundp 'tq-course-directory)
-				     tq-course-directory)
-				tq-course-directory)
-			       ((and (boundp 'ta-course-dir)
-				     ta-course-dir)
-				ta-course-dir))))
+;; (load-file (expand-file-name  "dft-book/dft.el"
+;; 			      (cond
+;; 			       ((and (boundp 'tq-course-directory)
+;; 				     tq-course-directory)
+;; 				tq-course-directory)
+;; 			       ((and (boundp 'ta-course-dir)
+;; 				     ta-course-dir)
+;; 				ta-course-dir))))
 
-(set-file-modes (expand-file-name tq-userid tq-root-directory) #o400)
-(dft-book-mode)
+;; (set-file-modes (expand-file-name tq-userid tq-root-directory) #o400)
+;; (dft-book-mode)
 
-(unless (package-installed-p 'git-messenger)
-  (package-refresh-contents)
-  (package-install 'git-messenger))
+;; (unless (package-installed-p 'git-messenger)
+;;   (package-refresh-contents)
+;;   (package-install 'git-messenger))
 
-(require 'kitchinhub)
-(load-file (expand-file-name "gilgamesh.el" starter-kit-dir))
+;; (require 'kitchinhub)
+;; (load-file (expand-file-name "gilgamesh.el" starter-kit-dir))
 (message "course setup.el loaded")
